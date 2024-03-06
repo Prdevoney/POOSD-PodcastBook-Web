c9c061b74eb45f4494ad8f5fd34f0bde81b67413
prdevoney <prdevo@gmail.com>
2024-03-06 01:47:43 -0500

    chagned react route to catch all & changed the users api file to podcast so its clear that it is for the use of the Listen API endpoints.

---
+++ routes/podcast.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+// Some sample function definitions for using the Listen Notes API to get podcast data
+router.post('/search', function(req, res, next) {
+  res.send('This may return a podcast name, description, photo, podcast ID, etc.');
+});
+
+router.post('/recomendations', function(req, res, next) {
+  res.send('Maybe we can have a feature to recommend podcasts based on the podcasts that the user has positive reviews on.');
+});
+
+module.exports = router;
