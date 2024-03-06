a5f3ff75ae5cd0f29c31f4243b88eb9ecbc92ffd
prdevoney <prdevo@gmail.com>
2024-03-06 01:53:45 -0500

    changed endpoint route

--- routes/podcast.js
+++ routes/podcast.js
@@ -6,7 +6,7 @@ router.post('/search', function(req, res, next) {
   res.send('This may return a podcast name, description, photo, podcast ID, etc.');
 });
 
-router.post('/recomendations', function(req, res, next) {
+router.post('/recommendations', function(req, res, next) {
   res.send('Maybe we can have a feature to recommend podcasts based on the podcasts that the user has positive reviews on.');
 });
 
