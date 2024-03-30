0ba1c23298cbe5ae3b30bac74490819e3dfac9ac
rdiorio <116819759+rdiorio@users.noreply.github.com>
2024-03-30 19:08:46 -0400

    Navbar colors update

--- client/src/Components/Explore/ExplorePodcasts.jsx
+++ client/src/Components/Explore/ExplorePodcasts.jsx
@@ -5,37 +5,25 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import './ExplorePage.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ExplorePodcasts =() =>{
   return (
-<Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              FUNNY
-            </Nav.Link>
+<Navbar expand="sm" className="navbar-container" /*bg="primary" data-bs-theme="dark"*/  >
+      <Container fluid >
+        <Navbar.Brand href="#">Podcast Book</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" style={{ height: '50px' }}/>
+        <Navbar.Collapse id="navbarScroll" >
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+            <Nav.Link href="#action1">Explore</Nav.Link>
+            <Nav.Link href="#action2">Account</Nav.Link>
           </Nav>
+          <Form.Select className = "me-3" style={{width: '110px'}} aria-label="Default select example">
+            <option value="1">Podcast</option>
+            <option value="2">Episode</option>
+          </Form.Select>
           <Form className="d-flex">
             <Form.Control
               type="search"
@@ -43,7 +31,7 @@ const ExplorePodcasts =() =>{
               className="me-2"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-light">Search</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
