4771615bec54014709f0629b9b2099555349bc2f
prdevoney <prdevo@gmail.com>
2024-03-31 23:48:11 -0400

    changed capitolization of file route in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import LoginSignup from './Components/LoginSignup/LoginSignup';
-import ExplorePodcasts from './Components/Explore/ExplorePodcasts';
+import LoginSignup from './components/LoginSignup/LoginSignup.jsx';
+import ExplorePodcasts from './components/Explore/ExplorePodcasts.jsx';
 // new added ->
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 // end of new added ->
-
 
 function App() {
 
