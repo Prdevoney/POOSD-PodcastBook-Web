b4af6b496f63e1cd172613c47a50414ff96c0d77
prdevoney <prdevo@gmail.com>
2024-04-16 15:03:07 -0400

    live api key

--- client/src/App.js
+++ client/src/App.js
@@ -3,47 +3,23 @@ import LoginSignup from './Components/LoginSignup/LoginSignup';
 import ExplorePodcasts from './Components/Explore/ExplorePodcasts';
 import Account from './Components/UserAccount/Account';
 import PodcastReview from './Components/Podcast/PodcastReview';
-// new added ->
-// import React, {useState} from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
-// end of new added ->
 
-//added now: 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
-// import { useHistory } from 'react-router-dom';
-import { useNavigate} from 'react-router-dom';
-// end
-
 
 function Main() {
   const location = useLocation();
   const showNavbar = location.pathname !== '/';
-  // const [searchResults, setSearchResults] = useState([]);
-  const navigate = useNavigate();
-
-  const handleSearch = (event) => {
-    // Work on the search API call here
-    event.preventDefault();
-    const searchTerm = event.target.elements[0].value;
-    const encodedSearchTerm = encodeURIComponent(searchTerm);
-
-    if (location.pathname === '/explore-podcasts') {
-      
-      navigate('/explore-podcasts', { state: { searchTerm: encodedSearchTerm } });
-    } else if (location.pathname === '/account') {
-      navigate('/explore-podcasts', { state: { searchTerm: encodedSearchTerm } });
-    }
-    
-  }
 
   return (
     <>
       {showNavbar && (
-        <Navbar expand="sm" className="navbar-container" sticky="top" /*bg="primary" data-bs-theme="dark"*/  >
+        <Navbar expand="sm" className="navbar-container" sticky="top" >
           <Container fluid >
           <img
                 src="/penguin.png" 
@@ -62,28 +38,6 @@ function Main() {
               <Navbar.Text className ="d-none d-sm-inline" style={{ color: 'inherit', fontSize: '20px', fontWeight: 'bold' , paddingRight: '30px'}}> | </Navbar.Text>
               <Nav.Link as={NavLink} to="/account" style={{ textDecoration: location.pathname === '/account' ? 'underline' : 'none', color: location.pathname === '/account' ? 'white' : 'inherit', fontSize: '20px' ,paddingRight: '30px' }}>Account</Nav.Link>
             </Nav>
-              
-{/*     Just commented out the search bar for now. my-2 my-lg-0
-
-            {location.pathname !== '/review-podcast' && (
-              <>
-             <Form.Select className = "me-3" style={{width: '110px'}} aria-label="Default select example">
-              <option value="1">Podcast</option>
-              <option value="2">Episode</option>
-              </Form.Select>
-             
-             <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                  onSubmit={handleSearch}
-                />
-                <Button variant="outline-light">Search</Button>
-              </Form>
-              </>
-              )} */}
 
             </Navbar.Collapse>
           </Container>
@@ -91,12 +45,9 @@ function Main() {
       )}
     </>
   )
-
 }
+
 function App() {
-
-
-
   return (
     <Router>
       <Main />
